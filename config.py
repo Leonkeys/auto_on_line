7e329057946b5c0ab2b8865fc87fb363cbed9761
unknown <liangluhao666@163.com>
2024-03-13 09:13:48 +0800

    config file type update

---
+++ config.py
@@ -0,0 +1,22 @@
+base = {
+    "base_uri": "/data/data3/zhangdq/online_data",
+    "dates": "",
+    "ekey": "",
+
+    "small_truck": {
+        "data_uri": "",
+        "version": "",
+        "report_data_uri": "",
+
+    },
+    "big_truck": {
+        "data_uri": "",
+        "version": "",
+        "report_data_uri": "",
+    },
+    "private_car": {
+        "data_uri": "",
+        "version": "",
+        "report_data_uri": "",
+    }
+}
